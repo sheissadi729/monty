90b70ecf0cafe915365b67ae55fdd07ea3297b52
sheissadi729 <sadicisse729@gmail.com>
2023-12-17 21:58:37 +0000

    implement nop opcode

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - doesn't do anything
+ * @stack: double pointer to the last element of the stack
+ * @line_number: line number
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
